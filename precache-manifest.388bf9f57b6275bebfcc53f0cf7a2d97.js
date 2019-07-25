295b0b03891a2802e5f1e31d19a15102b4415c84
Ashley <aparker@riskalyze.com>
2019-07-25 10:37:44 -0400

    Updates

---
+++ precache-manifest.388bf9f57b6275bebfcc53f0cf7a2d97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eacf67ff602b3f8f79adb3c73ae5fd5",
+    "url": "/cr_portfolio/index.html"
+  },
+  {
+    "revision": "107f771e2b5597ad0e27",
+    "url": "/cr_portfolio/static/css/main.dd901809.chunk.css"
+  },
+  {
+    "revision": "7eb97bc26795816301eb",
+    "url": "/cr_portfolio/static/js/2.a2bfebbc.chunk.js"
+  },
+  {
+    "revision": "107f771e2b5597ad0e27",
+    "url": "/cr_portfolio/static/js/main.5d1f44de.chunk.js"
+  },
+  {
+    "revision": "db4e4495a4000d1313f1",
+    "url": "/cr_portfolio/static/js/runtime~main.166d1789.js"
+  }
+]);
